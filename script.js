7da6685511e2af6ef63b88c564f1ec177da7b1a2
Jonathan Cedeño <jony.cede@gmail.com>
2021-03-29 14:13:39 +0200

    added functionality of switch template

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 // Global - 1 or 2 or 3 - 
-var indexPage = 0;
+var indexPage = 1;
 
 // Template user-logging - 1 -
-var templateUserLogging = document.getElementById("user-logging");
-var templateClones = templateUserLogging.content.cloneNode(true);
+var templateElement = document.getElementById("user-logging-page");
+var templateClon = templateElement.content.cloneNode(true);
+document.getElementById('containerAllLeft').appendChild(templateClon);
+document.getElementById("start-user-logging-button").addEventListener("click", TemplateSwitch);
 
-document.getElementById('containerAllLeft').appendChild(templateClones);
+// This function changes the container-left according to the page and templates
+function TemplateSwitch(){
+    var containerLeft = document.getElementById("containerAllLeft");
+    
+    // The container.left content will be removed
+    while (containerLeft.firstChild) {
+        containerLeft.removeChild(containerLeft.firstChild);
+    }
 
-// Template game-screen - 2 -
-// var templateGameScreen = document.getElementById("game-screen");
-// var templateClones = templateGameScreen.content.cloneNode(true);
+    // The template will be inserted in container-left according to the current page.
+    switch (indexPage) {
+        case 1:
+            templateElement = document.getElementById("game-page");
+            templateClon = templateElement.content.cloneNode(true);
+            document.getElementById("containerAllLeft").appendChild(templateClon);
+            document.getElementById("stop-game-button").addEventListener("click", TemplateSwitch);
 
-// document.getElementById('containerAllLeft').appendChild(templateClones);
+            indexPage++;
+            break;
+        case 2:
+            templateElement = document.getElementById("final-score-page");
+            templateClon = templateElement.content.cloneNode(true);
+            document.getElementById("containerAllLeft").appendChild(templateClon);
+            document.getElementById("play-again-button").addEventListener("click", TemplateSwitch);
 
-// Template final-score - 3 -
-// var templateFinalScore = document.getElementById("final-score");
-// var templateClones = templateFinalScore.content.cloneNode(true);
+            indexPage++;
+            break;
+        case 3:
+            templateElement = document.getElementById("user-logging-page");
+            templateClon = templateElement.content.cloneNode(true);
+            document.getElementById("containerAllLeft").appendChild(templateClon);
+            document.getElementById("start-user-logging-button").addEventListener("click", TemplateSwitch);
 
-// document.getElementById('containerAllLeft').appendChild(templateClones);
+            indexPage = 1;
+            break;
+    }
+}

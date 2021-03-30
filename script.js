b35bd5bc68a555971173132c77de25a828b3c5ad
Jonathan Cedeño <jony.cede@gmail.com>
2021-03-30 12:57:19 +0200

    implemented the functionality of game

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Global - 1 or 2 or 3 - 
 var indexPage = 1;
 
+// Globals for timer
+var timer = 0;
+var intervalElement;
+
 // Template user-logging - 1 -
 var templateElement = document.getElementById("user-logging-page");
 var templateClon = templateElement.content.cloneNode(true);
@@ -22,7 +26,7 @@ function TemplateSwitch(){
             templateElement = document.getElementById("game-page");
             templateClon = templateElement.content.cloneNode(true);
             document.getElementById("containerLeft").appendChild(templateClon);
-            document.getElementById("stop-game-button").addEventListener("click", TemplateSwitch);
+            document.getElementById("start-game-button").addEventListener("click", StartGame);
 
             indexPage++;
             break;
@@ -43,4 +47,49 @@ function TemplateSwitch(){
             indexPage = 1;
             break;
     }
+}
+
+/* This function starts the game */
+function StartGame() {
+    /* Timer reseted from previous game */
+    timer = 0;
+    
+    /* Button deleted and "Get Ready" message added and shown */
+    var startButtonElement = document.getElementById("start-game-button");
+    startButtonElement.remove();
+    document.getElementById("containerLeft").insertAdjacentHTML("afterbegin","<div id='get-ready-message'><h2>Get Ready...</h2></div>");
+
+    /* This function is called after 1 seconds */
+    setTimeout(RandomShown, 1000);
+}
+
+/* This function generates the random number and runs the finish game */
+function RandomShown(){
+    var randomNumber = Math.floor(Math.random() * 10000);
+    setTimeout(FinishGame, randomNumber);
+}
+
+/* This function finishes the game */
+function FinishGame(){
+    /* "Get Ready" message deleted and stop button added and shown */
+    var divGetReadyElement = document.getElementById("get-ready-message");
+    divGetReadyElement.remove();
+    document.getElementById("containerLeft").insertAdjacentHTML("afterbegin","<button id='stop-game-button' class='gameButtonsClass'>Stop game</button>");
+    document.getElementById("stop-game-button").addEventListener("click", TemplateSwitch);
+    document.getElementById("stop-game-button").addEventListener("click", SaveTheTime);
+
+    intervalElement = setInterval(CountMiliseconds,10);
+}
+
+/* This function counts the miliseconds */
+function CountMiliseconds(){
+    timer++;
+}
+
+/* This function saves the time spent and shown in the last page */
+function SaveTheTime(){
+    clearInterval(intervalElement);
+    timer = timer/100;
+
+    document.getElementById("score-data").innerHTML = timer + " seconds";
 }
